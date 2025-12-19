82780230007a26449d79660367cb29af40e68066
sbarzani466 <shahrad056@gmail.com>
2025-12-19 19:44:40 +0300

    Add files via upload

---
+++ Updated_version.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.updated_version;
+
+/**
+ *
+ * @author danial
+ */
+public class Updated_version {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! thisi updated version");
+    }
+}
