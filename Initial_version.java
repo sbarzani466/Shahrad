2c57bb232929636f25caa6b3b51bf58e664b843d
sbarzani466 <shahrad056@gmail.com>
2025-12-19 19:43:58 +0300

    Add files via upload

---
+++ Initial_version.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.initial_version;
+
+/**
+ *
+ * @author danial
+ */
+public class Initial_version {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! this is a file called initial version");
+    }
+}
